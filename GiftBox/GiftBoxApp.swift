c5f10110f3a3e33766828b6c34a47957658f3b7d
Lizsa <liza@finch-melrose.com>
2022-10-12 12:59:25 +0300

    Initial Commit

---
+++ GiftBox/GiftBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftBoxApp.swift
+//  GiftBox
+//
+//  Created by Lisa on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct GiftBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
